1ff723b60878673870d681d56dcaf9e8d4b5382b
Tanto S <tsugiar@hotmail.com>
2018-11-27 19:21:49 -0800

    Update on training model for traffic classification

--- TrainingData/ModelTraining.py
+++ TrainingData/ModelTraining.py
@@ -12,31 +12,113 @@ import keras.losses
 import glob
 
 
+def exploration_plot(data):
+    # Randomly pick data and plot
+    data_size = data['Xtrain'].shape[0]
+    data_idx = random.sample(range(0, data_size), 20)
+    plt.figure()
+    for seq, idx in enumerate(data_idx):
+        orig_img = data['Xtrain'][idx]
+        tmp_img = np.zeros_like(orig_img)
+
+        # Flip second and third color element of orig_img
+        tmp_img[:, :, 0] = orig_img[:, :, 2]
+        tmp_img[:, :, 1] = orig_img[:, :, 1]
+        tmp_img[:, :, 2] = orig_img[:, :, 0]
+
+        plt.subplot(4, 5, seq + 1)
+        plt.imshow(tmp_img)
+
+        # Check state of traffic-light
+        state = data['YLabel'][idx]
+        if (state == 0 ):
+            tf_color = 'red / yellow'
+        elif state == 1:
+            tf_color = 'green'
+        else:
+            tf_color = 'unknown'
+
+        # Check distance from ego to traffic light
+        dist = data['EgoTrafficDist'][idx]
+
+        title_str = 'Color : %s , Dist : %d m' % (tf_color, dist)
+        plt.title(title_str)
+        plt.axis('off')
+    plt.show()
+
+def split_data(whole_data, split_frac):
+    data_len = whole_data['Xtrain'].shape[0]
+
+    # Shuffle data
+    shuffle_idx = random.sample(range(0,data_len),data_len)
+    whole_data['Xtrain'] = whole_data['Xtrain'][shuffle_idx,:,:,:]
+    whole_data['YLabel'] = [whole_data['YLabel'][val] for val in shuffle_idx]
+    whole_data['EgoTrafficDist'] = [whole_data['EgoTrafficDist'][val] for val in shuffle_idx]
+
+    # Split data into training and testing
+    final_train_idx = int(np.floor(split_frac * data_len))
+    train_idx_array = range(0,final_train_idx)
+    test_idx_array  = range(final_train_idx, data_len)
+
+    train_data = {}
+    test_data  = {}
+
+    train_data['Xtrain'] = whole_data['Xtrain'][train_idx_array,:,:,:]
+    train_data['YLabel'] = [whole_data['YLabel'][val] for val in train_idx_array]
+    train_data['EgoTrafficDist'] = [whole_data['EgoTrafficDist'][val] for val in train_idx_array]
+
+    test_data['Xtrain'] = whole_data['Xtrain'][test_idx_array,:,:,:]
+    test_data['YLabel'] = [whole_data['YLabel'][val] for val in test_idx_array]
+    test_data['EgoTrafficDist']  = [whole_data['EgoTrafficDist'][val] for val in test_idx_array]
+
+    return (train_data, test_data)
+
+
+
 BATCH_SIZE = 32
-num_classes = 3
+num_classes = 2
 
 # Initialize dictionary var that will contain collection of
 # training data
+data_samples = {}
 train_samples = {}
+test_samples = {}
 
 # Scan for all available training data
-training_file_list = glob.glob('Training2018*')
+data_file_list = glob.glob('Training2018*')
 
-for training_file in training_file_list:
-    fid  = open(training_file,'rb')
+for data_file in data_file_list:
+    fid  = open(data_file,'rb')
     temp_data = pickle.load(fid)
 
-    # Store it inside dictionary
-    if train_samples.has_key('Xtrain'):
-        train_samples['Xtrain'] = np.append(train_samples['Xtrain'], temp_data['Xtrain'], axis=0)
-        train_samples['YLabel'].extend(temp_data['YLabel'])
-        train_samples['EgoTrafficDist'].extend(temp_data['EgoTrafficDist'])
-    else:
-        train_samples = temp_data
+    # Iterate over each of label on temp_data, if it is yellow or red set it to 0,
+    # if it is green set it to 1
+    temp_data['YLabel'] = [0 if (item == 0 or item == 1) else 1 for item in temp_data['YLabel']]
 
-# compile and train the model using the generator function
-# fid = open('Training2018112363121','rb')
-# train_samples = pickle.load(fid)
+    fid.close()
+
+    # Store it inside dictionary
+    if data_samples.has_key('Xtrain'):
+        data_samples['Xtrain'] = np.append(data_samples['Xtrain'], temp_data['Xtrain'], axis=0)
+        data_samples['YLabel'].extend(temp_data['YLabel'])
+        data_samples['EgoTrafficDist'].extend(temp_data['EgoTrafficDist'])
+    else:
+        data_samples = temp_data
+
+# Split data into training and test/validation
+train_samples, test_samples = split_data(data_samples, 0.9)
+#train_samples = data_samples
+#import copy
+#test_samples = copy.copy(data_samples)
+
+exploration_plot(train_samples)
+
+
+# Shuffle and split data based on split_frac, where :
+# training-data = split_frac *  whole_data
+# test-data     = ( 1 - split_frac) * whole_data
+
+
 
 def resize_image(image):
     return ktf.image.resize_images(image, (600, 800))
@@ -100,7 +182,7 @@ def generator(samples, batch_size=BATCH_SIZE):
             yield(img_processed, y_train)
 
 train_generator = generator(train_samples, batch_size=BATCH_SIZE)
-validation_generator = generator(train_samples, batch_size=BATCH_SIZE)
+validation_generator = generator(test_samples, batch_size=len(test_samples['YLabel']))
 
 row, col, ch = train_samples['Xtrain'].shape[1:]
 
